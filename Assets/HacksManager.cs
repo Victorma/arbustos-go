8dfcf687b1e39ca4e42b31e69887124885f04467
Victorma <victorma-fire@hotmail.com>
2020-02-18 16:19:07 +0100

    Added hacks and text exporter

---
+++ Assets/HacksManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uAdventure.Core;
+using uAdventure.Runner;
+
+public class HacksManager : MonoBehaviour {
+
+	public void IrARecogida()
+    {
+        GUIManager.Instance.ShowConfigMenu();
+        Game.Instance.Execute(new EffectHolder(new Effects()
+        {
+            new TriggerSceneEffect("Plaza", 0, 0),
+            new ActivateEffect("Intro"),
+            new ActivateEffect("Alive"),
+            new ActivateEffect("Normal"),
+            new ActivateEffect("Recogida")
+        }));
+    }
+
+    public void IrAFarmacia()
+    {
+        GUIManager.Instance.ShowConfigMenu();
+        Game.Instance.Execute(new EffectHolder(new Effects()
+        {
+            new TriggerSceneEffect("FacultadFarmacia", 0, 0),
+            new ActivateEffect("Intro"),
+            new ActivateEffect("Alive"),
+            new ActivateEffect("Normal"),
+            new ActivateEffect("Recogida")
+        }));
+    }
+
+    public void IrAMedicina()
+    {
+        GUIManager.Instance.ShowConfigMenu();
+        Game.Instance.Execute(new EffectHolder(new Effects()
+        {
+            new TriggerSceneEffect("FacultadMedicina", 0, 0),
+            new ActivateEffect("Intro"),
+            new ActivateEffect("Alive"),
+            new ActivateEffect("Normal"),
+            new ActivateEffect("Recogida")
+        }));
+    }
+
+    public void IrAlFinal()
+    {
+        GUIManager.Instance.ShowConfigMenu();
+        Game.Instance.Execute(new EffectHolder(new Effects()
+        {
+            new TriggerSceneEffect("Ending", 0, 0),
+            new ActivateEffect("Intro"),
+            new ActivateEffect("Alive"),
+            new ActivateEffect("Normal"),
+            new ActivateEffect("Recogida"),
+            new TriggerConversationEffect("BatallaFinal")
+        }));
+    }
+}
